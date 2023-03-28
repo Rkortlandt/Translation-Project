54f21cf99caa170a36b5c1b62d46c8f565b512c2
Rkortlandt <93385295+Rkortlandt@users.noreply.github.com>
2023-03-28 02:41:21 +0000

    added moduals

---
+++ public/modules/graph.js
@@ -0,0 +1,71 @@
+/**
+ * @param gridSpacing Spacing of the grid 
+ * @param stx Static canvas element
+ */
+export function initGraph(gridSpacing, stx) {
+	//Main Lines
+	console.log('initGraph Func Called');
+
+	stx.translate(484/2, 484/2);
+	stx.strokeStyle = "#000000"
+	stx.beginPath()
+	//line to right 
+	stx.moveTo(0,0);
+	stx.lineTo(242-22, 0);
+	stx.stroke();
+	//line to left  
+	stx.moveTo(0,0);
+	stx.lineTo(-242+22, 0);
+	stx.stroke();
+	//line down    
+	stx.moveTo(0,0);
+	stx.lineTo(0, 242-22);
+	stx.stroke();
+	//line up    
+	stx.moveTo(0,0);
+	stx.lineTo(0, -242+22);
+	stx.stroke();
+
+	//Black line markers----------------------------------------------------    
+	for (let i = -10 * gridSpacing; i <= 10 * gridSpacing; i++) {
+		let y = (-22 / gridSpacing) * i;
+		let x = -10 / gridSpacing;
+		let x2 = 10 / gridSpacing;
+		
+		stx.moveTo(x,y);
+		stx.lineTo(x2,y);
+		stx.stroke();
+	}
+	for (let i = -10 * gridSpacing; i <= 10 * gridSpacing; i++) {
+		let x = (-22 / gridSpacing) * i;
+		let y = -10 / gridSpacing;
+		let y2 = 10 / gridSpacing;
+		
+		stx.moveTo(x,y);
+		stx.lineTo(x,y2);
+		stx.stroke();
+	}
+		
+	stx.resetTransform()
+	//All the little gray things-------------------------------------------
+	for (let i = 0; i <= 20; i++) {
+		if (i !== 10) {
+			var ydash = 22 + i*22 ;
+			for (let i = 0; i <= 20; i++) {
+				if (i !== 10){
+					let x = 20 + i * 22
+					let x2 = 24 + i * 22
+					stx.beginPath();
+					stx.strokeStyle = "#bdbdbd"
+					stx.moveTo(x, ydash);
+					stx.lineTo(x2, ydash);
+					stx.stroke();
+				} else {
+					console.log("X skipped")
+				}
+			}
+		} else {
+			console.log("Y skipped")
+		}
+	}
+}
---
+++ public/modules/line.js
@@ -0,0 +1,8 @@
+export default class line {
+ 
+    constructor (pointA, pointB) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+    }
+    type = "line";
+}
